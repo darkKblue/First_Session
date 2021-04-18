01b99e842da15c6fd0374f8d0659890c338a3bd8
darkKblue <82527736+darkKblue@users.noreply.github.com>
2021-04-18 22:06:28 +0430

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,11 @@
+num_first=int(input())
+num_second=int(input())
+num_third=int(input())
+jam=num_first+num_second+num_third
+
+if (num_first and num_second and num_third) >0 and jam==180:
+
+    print("yes")
+else:
+
+    print("No")
